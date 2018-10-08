2612d30568db14d64f2a459ff8f92e5659084d91
EduarteCliff <37478960+EduarteCliff@users.noreply.github.com>
2018-10-08 18:23:00 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -34,7 +34,7 @@
 										  );
 			}
 			if($str["hash_pass"]==$hash_pass){
-				include '../functions/post.php';
+				/*include '../functions/post.php';
 				$chaptcha=post("https://www.google.com/recaptcha/api/siteverify","secret=6LeK-3IUAAAAABRzTjaawbvdZVdcbBSKzZthftES&response=".$_POST["g-recaptcha-response"]);
 				$chaptcha=json_decode($chaptcha,true);
 				if(!$chaptcha["success"]){
@@ -47,14 +47,14 @@
 								  )</script>";
 					echo file_get_contents("../templates/index_body.tpl");
 					die("");
-				}
+				}*/
 				echo "<body>";
               	echo "<script>sweetAlert('干得漂亮！', '登录成功！','success')</script>";
 				echo file_get_contents("../templates/index_body_loggedin.tpl");
 				setcookie("user",$hash_user,time()+2592000,"/");
 				setcookie("pass",$hash_pass,time()+2592000,"/");
 			} elseif(!isset($str["hash_pass"])){
-				include '../functions/post.php';
+				/*include '../functions/post.php';
 				$chaptcha=post("https://www.google.com/recaptcha/api/siteverify","secret=6LeK-3IUAAAAABRzTjaawbvdZVdcbBSKzZthftES&response=".$_POST["g-recaptcha-response"]);
 				$chaptcha=json_decode($chaptcha,true);
 				if(!$chaptcha["success"]){
@@ -67,7 +67,7 @@
 								  )</script>";
 					echo file_get_contents("../templates/index_body.tpl");
 					die("");
-				}
+				}*/
 				echo file_get_contents("../templates/index_body.tpl");
 				require_once("../phpmailer/class.phpmailer.php");
 				require_once("../phpmailer/class.smtp.php");
